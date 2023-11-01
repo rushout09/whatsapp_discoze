fae8563a2c71ee13881da4fbdc96e096dfe19faa
Rushabh <rushabh.agarwal9@gmail.com>
2023-11-01 11:37:15 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+
+@app.route('/verification', methods=['GET'])
+def verification():
+    data = request.get_json()
+    if data.get("hub.mode") == "subscribe" and data.get("hub.verify_token") == "test":
+        return data.get("hub.challenge"), 200
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
